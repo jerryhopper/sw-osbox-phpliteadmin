101c6ab48b66bfff8f36ba4d8f42d9542c6a93b7
JerryHopper <hopper.jerry@gmail.com>
2020-11-24 17:07:45 +0100

    Update phpliteadmin.config.php

--- code/phpliteadmin.config.php
+++ code/phpliteadmin.config.php
@@ -22,7 +22,7 @@ $subdirectories = false;
 //if any of the databases do not exist as they are referenced by their path, they will be created automatically
 $databases = array(
 	array(
-		'path'=> 'osbox.db',
+		'path'=> '/db/osbox.db',
 		'name'=> 'OSBox'
 	),
 );
